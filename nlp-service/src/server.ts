787dbb9d4433136fd3cff800e79b9c44c30c3183
Jaroslaw Butajlo <jaroslaw.butajlo@allbright.io>
2019-05-25 17:33:02 +0200

    add https

--- nlp-service/src/server.ts
+++ nlp-service/src/server.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as http from 'http';
+import * as https from 'https';
 import ga from './assistant';
 import { createConnection, getMongoRepository, Repository, getRepository } from 'typeorm';
 import { addMenu } from './service';
 import { MenuEntity, PizzaEntity } from './data/entities';
+import { readFileSync } from 'fs';
 
 // createConnection({
 //     type: "mongodb",
@@ -23,10 +26,10 @@ import { MenuEntity, PizzaEntity } from './data/entities';
 //     app.post('/update_menu', async (req, res) => {
 //         const menu = menuRepository.create(req.body)
 //         const saved = await menuRepository.save(menu)
-        
+
 //         res.json(saved)
 //     })
-    
+
 //     const PORT = process.env.NLP_PORT || 3000
 //     app.listen(PORT).on('listening', () => {
 //         console.log(`Server is running at: http://localhost:${PORT}`)
@@ -34,15 +37,27 @@ import { MenuEntity, PizzaEntity } from './data/entities';
 // })
 
 function bootstrap() {
-    const app = express()
-    app.use(bodyParser.json())
+	const key = readFileSync('./sslcert/server.key', 'utf8');
+	const cert = readFileSync('./sslcert/server.cert', 'utf8');
 
-    app.post('/nlp', ga)
+	const credentials = { key, cert };
 
-    const PORT = process.env.NLP_PORT || 3000
-    app.listen(PORT).on('listening', () => {
-        console.log(`Server is running at: http://localhost:${PORT}`)
-    })
+	const app = express();
+	app.use(bodyParser.json());
+
+	app.post('/nlp', ga);
+
+	// app.listen(3000).on('listening', () => {
+	// 	console.log(`Server is running at: http://localhost:${PORT}`);
+	// });
+	// http.createServer(app).listen(3000).on('listening', () => {
+	// 	// 	console.log(`Server is running at: http://localhost:${PORT}`);
+	// 	console.log('Server is running on port 3000');
+	// });
+
+	https.createServer(credentials, app).listen(5000).on('listening', () => {
+		console.log('Server is running on port 5000');
+	});
 }
 
-bootstrap()
+bootstrap();
