28c6ac2d4f24066dd146d62dd5191c303997ffc1
Jaroslaw Butajlo <jaroslaw.butajlo@allbright.io>
2019-05-25 17:05:33 +0200

    server with mocks only

--- nlp-service/src/server.ts
+++ nlp-service/src/server.ts
@@ -5,30 +5,44 @@ import { createConnection, getMongoRepository, Repository, getRepository } from 
 import { addMenu } from './service';
 import { MenuEntity, PizzaEntity } from './data/entities';
 
-createConnection({
-    type: "mongodb",
-    host: "localhost",
-    port: 27017,
-    database: "rso_test",
-    useNewUrlParser: true,
-    entities: [MenuEntity ]
-}).then(connection => {
-    const menuRepository = getRepository(MenuEntity)
+// createConnection({
+//     type: "mongodb",
+//     host: "localhost",
+//     port: 27017,
+//     database: "rso_test",
+//     useNewUrlParser: true,
+//     entities: [MenuEntity ]
+// }).then(connection => {
+//     const menuRepository = getRepository(MenuEntity)
 
+//     const app = express()
+//     app.use(bodyParser.json())
+
+//     app.post('/nlp', ga)
+
+//     app.post('/update_menu', async (req, res) => {
+//         const menu = menuRepository.create(req.body)
+//         const saved = await menuRepository.save(menu)
+        
+//         res.json(saved)
+//     })
+    
+//     const PORT = process.env.NLP_PORT || 3000
+//     app.listen(PORT).on('listening', () => {
+//         console.log(`Server is running at: http://localhost:${PORT}`)
+//     })
+// })
+
+function bootstrap() {
     const app = express()
     app.use(bodyParser.json())
 
     app.post('/nlp', ga)
 
-    app.post('/update_menu', async (req, res) => {
-        const menu = menuRepository.create(req.body)
-        const saved = await menuRepository.save(menu)
-        
-        res.json(saved)
-    })
-    
     const PORT = process.env.NLP_PORT || 3000
     app.listen(PORT).on('listening', () => {
         console.log(`Server is running at: http://localhost:${PORT}`)
     })
-})
+}
+
+bootstrap()
